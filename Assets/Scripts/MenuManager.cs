ffe1cb3924e41a5a3f3a96939c0e0077f82152d2
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2017-03-02 16:31:56 +0000

    Began the framework for upgrades
    Just added the scene and a few animations ready for the upgrades to be
    implemented

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;  //Added the using scenemanagement namespace because this is how you have to do it now.
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
+
+    bool controlsShowing = false;
 
     //Load a level with the passed in integer.
     public void LoadLevel(int levelToLoad)
@@ -19,10 +22,11 @@ public class MenuManager : MonoBehaviour {
     }
 
     //Loads the controls menu.
-    public void LoadControls()
+    public void LoadControls(bool unload)
     {
         //Finds the preset animation for the controls.
         Animation controlsAnim = GameObject.Find("Canvas").transform.Find("ControlsPanel").GetComponent<Animation>();
+
         //Plays the controls animation which heightens the alpha of the canvas group.
         controlsAnim.Play();
     }
