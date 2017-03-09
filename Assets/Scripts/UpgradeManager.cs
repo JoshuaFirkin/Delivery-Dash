49411740b21997568b677d989e0dfe7a70e2be62
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2017-03-09 13:59:15 +0000

    Added More Framework

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,21 +1,79 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 
 public class UpgradeManager : MonoBehaviour {
 
+    int speedBonus = 0;
+    int brakingBonus = 0;
+    int deliveryBonus = 0;
+
+    int[] upgradeLevels = new int[3] { 0, 0, 0 };
+    int[] prices = new int[3] { 100, 200, 300 };
+
+    private Text savingsText;
 
 
     void Start()
     {
-        
+        savingsText = GameObject.Find("Canvas").transform.FindChild("SavingsTxt").GetComponent<Text>();
+        savingsText.text = "$" + PlayerPrefs.GetInt("TotalSavings");
     }
 
-    public void UpgradeSpeed()
+
+    public void Upgrade(char upgrade)
     {
+        switch (upgrade)
+        {
+            case 's':
+                if (upgradeLevels[0] < 3)
+                {
+                    UpSpeed();
+                    savingsText.text = ("$" + MakeTransaction(prices[upgradeLevels[0]]));
+                }
+                break;
 
+
+            case 'b':
+                if (upgradeLevels[1] < 3)
+                {
+                    UpBraking();
+                    MakeTransaction(prices[upgradeLevels[1]]);
+                }
+                break;
+
+
+            case 'd':
+                if (upgradeLevels[2] < 3)
+                {
+                    UpDelivery();
+                    MakeTransaction(prices[upgradeLevels[2]]);
+                }
+                break;
+        }
     }
+
+
+    void UpSpeed()
+    {
+        speedBonus += 10;
+        PlayerPrefs.SetInt("SpeedBonus", speedBonus);
+    }
+
+    void UpBraking()
+    {
+        brakingBonus += 10;
+        PlayerPrefs.SetInt("BrakingBonus", brakingBonus);
+    }
+
+    void UpDelivery()
+    {
+        deliveryBonus += 10;
+        PlayerPrefs.SetInt("DeliveryBonus", deliveryBonus);
+    }
+
 
 
     int MakeTransaction(int amount)
